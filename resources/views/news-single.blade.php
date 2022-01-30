89ec9f0e7ecb7347d1a7e3624652f506eeeb0c9b
Simon Karapetyan <simonkarapetyan605@gmail.com>
2022-01-30 22:46:49 +0400

    more bugs fixed

--- resources/views/news-single.blade.php
+++ resources/views/news-single.blade.php
@@ -11,7 +11,7 @@
             <div class="news__home_cont">
                 <div class="wrapper">
                     <h2 class="news__home_title">
-                        {{ $news->{'title_'.app()->getLocale()} }}
+                        {{ $news->{'title_' . app()->getLocale()} }}
                     </h2>
                 </div>
             </div>
@@ -21,7 +21,7 @@
                 <div class="new-info__cont">
                     <span class="new-info__cont_date">{{ $news->date }}</span>
                     <div class="text-18 new-info__text">
-                        {!! $news->{'description_'.app()->getLocale()} !!}
+                        {!! $news->{'description_' . app()->getLocale()} !!}
                     </div>
                 </div>
             </div>
@@ -30,25 +30,38 @@
             <div class="wrapper">
                 <div class="new-images_cont">
                     <div class="new-images__top flex">
-                        <div class="swiper">
-                            <div class="swiper-wrapper">
-                                @foreach($news->imgs as $img)
-                                    <div class="swiper-slide new-top__img">
+                        @if (count($news->imgs) > 2)
+                            <div class="swiper">
+                                <div class="swiper-wrapper">
+                                    @foreach ($news->imgs as $img)
+                                        <div class="swiper-slide new-top__img">
+                                            <a href="{{ Storage::url($img->img) }}" target="_blank">
+                                                <img class="img" src="{{ Storage::url($img->img) }}"
+                                                    alt="{{ Storage::url($img->id) }}">
+                                            </a>
+                                        </div>
+                                    @endforeach
+                                    <div class="swiper-button-prev swiper__arrow"></div>
+                                    <div class="swiper-button-next swiper__arrow"></div>
+                                </div>
+                            </div>
+                        @else
+                            @foreach ($news->imgs as $img)
+                                <div class="new-top__imgs flex">
+                                    <div class="new-top__img">
                                         <a href="{{ Storage::url($img->img) }}" target="_blank">
                                             <img class="img" src="{{ Storage::url($img->img) }}"
                                                 alt="{{ Storage::url($img->id) }}">
                                         </a>
                                     </div>
-                                @endforeach
-                            </div>
-                            <div class="swiper-button-prev swiper__arrow"></div>
-                            <div class="swiper-button-next swiper__arrow"></div>
-                        </div>
+                                </div>
+                            @endforeach
+                        @endif
                     </div>
                 </div>
             </div>
         </section>
-        
+
         @include('includes.news.last-news')
     </main>
 @endsection
