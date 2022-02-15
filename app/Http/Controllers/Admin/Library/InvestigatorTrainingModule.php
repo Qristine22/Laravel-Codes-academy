bb3ad6d05b530582acff13b1e2caa4f9e1b18652
Simon Karapetyan <simonkarapetyan605@gmail.com>
2022-02-15 19:49:35 +0400

    added new library model

---
+++ app/Http/Controllers/Admin/Library/InvestigatorTrainingModule.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Library;
+
+use App\Http\Controllers\Controller;
+use App\Models\Library;
+use Illuminate\Http\Request;
+
+class InvestigatorTrainingModule extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Library $library)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Library $library)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Library $library)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Library $library)
+    {
+        //
+    }
+}
