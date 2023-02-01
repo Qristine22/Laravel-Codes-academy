a1d7f6acb8ac921f2a233d892ad823c9dfd4d3e1
Simon Karapetyan <simon.karapetyan@esterox.am>
2023-02-01 15:21:13 +0400

    Fixed news images small size issue

--- resources/views/news-single.blade.php
+++ resources/views/news-single.blade.php
@@ -32,7 +32,7 @@
             <div class="wrapper">
                 <div class="new-images_cont">
                     <div class="new-images__top gallery__cont flex">
-                    @if (count($imgs) > 2)
+                        @if (count($imgs) > 2)
                             <div class="swiper">
                                 <div class="swiper-wrapper">
                                     @foreach ($imgs as $img)
@@ -51,29 +51,10 @@
 
                                 <div class="swiper-button-next swiper__arrow"></div>
                             </div>
-                       @elseif (count($imgs) == 2)
-                            <div class="swiper">
-                                <div class="swiper-wrapper">
-                                    @foreach ($imgs as $img)
-                                        <div class="swiper-slide mx-2 new-top__img">
-                                            <div class="gallery__item flex">
-                                                <a class="gallery__top" data-fancybox="gallery"
-                                                   href="{{ Storage::url($img->img) }}">
-                                                    <img class="gallery__img img" src="{{ Storage::url($img->img) }}"
-                                                         alt="1">
-                                                </a>
-                                            </div>
-                                        </div>
-                                    @endforeach
-                                </div>
-                                <div class="swiper-button-prev swiper__arrow"></div>
-
-                                <div class="swiper-button-next swiper__arrow"></div>
-                            </div>
                         @else
                             @foreach ($imgs as $img)
                                 <div class="new-top__imgs">
-                                    <div class="new-top__img mx-auto">
+                                    <div class="new-top__img">
                                         <div class="gallery__item">
                                             <a class="gallery__top" data-fancybox="gallery"
                                                href="{{ Storage::url($img->img) }}">
@@ -82,7 +63,6 @@
                                             </a>
                                         </div>
                                     </div>
-
                                 </div>
                             @endforeach
                         @endif
@@ -101,32 +81,14 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
     @if (count($imgs) > 2)
-    <script>
-        let mySwiper = new Swiper('.swiper', {
-            slidesPerView: 3,
-            direction: 'horizontal',
-
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-
-            breakpoints: {
-                0: {
-                    slidesPerView: 2,
-                },
-                601: {
-                    slidesPerView: 3,
+        <script>
+            setInterval(function () {
+                let url = document.URL
+                if (url.includes('gallery')) {
+                    let item = url.split('-')[1] * 1
+                    mySwiper.slideTo(item, true, true);
                 }
-            }
-        });
-        setInterval(function () {
-            let url = document.URL
-            if (url.includes('gallery')) {
-                let item = url.split('-')[1] * 1
-                mySwiper.slideTo(item, true, true);
-            }
-        }, 500)
-    </script>
+            }, 500)
+        </script>
     @endif
 @endsection
