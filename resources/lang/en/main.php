64048e1f2e0cb436369cbc35f875e6bfd111f1e7
gevorg-daghlaryan <gevorg.daghlaryan@esterox.am>
2024-10-07 14:31:13 +0400

    Added instrument section top to header

--- resources/lang/en/main.php
+++ resources/lang/en/main.php
@@ -6,4 +6,6 @@ return [
     'download' => 'download',
     'info-is-available' => 'Information is available in Armenian',
     'browser-does-not-support' => 'Your browser does not support the video tag.',
+    'color-mode' => 'WEBSITE COLOR',
+    'font-size' => 'TEXT FONT SIZE',
 ];
